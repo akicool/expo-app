f22fcbd12f8134234c79724fdf9ab1d7f47deda4
David <liid05754@gmail.com>
2023-10-23 23:08:41 +0300

    app commit

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, Text, View } from "react-native";
 
-export default function App() {
+export default MobileApp = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View className="flex-1 w-full flex justify-center items-center bg-black">
+        <Button title="lollllll" className='px-10'/>
+      </View>
+    </>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
